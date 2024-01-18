ef2fe1abe9959de0422ead6cd7abf3e778abbebb
basilysf1709 <basilyusuf1709@gmail.com>
2024-01-18 18:28:02 -0500

    init: start project

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "First Year checklist",
+  description: "Checklist for first years to go through",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
